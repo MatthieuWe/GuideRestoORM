39d4070b11381432ff451b5eb098671cf81cda71
maria.ramirez <maria.ramirez@he-arc.ch>
2025-11-25 09:06:55 +0100

    take back my shit

--- src/main/java/ch/hearc/ig/guideresto/business/IBusinessObject.java
+++ src/main/java/ch/hearc/ig/guideresto/business/IBusinessObject.java
@@ -3,7 +3,6 @@ package ch.hearc.ig.guideresto.business;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 
-@Inheritance(strategy= InheritanceType.TABLE_PER_CLASS)
 public interface IBusinessObject {
     Integer getId();
 }
