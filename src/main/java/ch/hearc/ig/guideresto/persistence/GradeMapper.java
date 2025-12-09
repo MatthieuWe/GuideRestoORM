d1dfc87fdad2f2f9ec6cbc2585d76bca2dcc357c
Matthieu Wehrli <matthieu.wehrli@he-arc.ch>
2025-12-09 10:00:39 +0100

    copied and adapted content of basic evaluations into evaluations criteria

--- src/main/java/ch/hearc/ig/guideresto/persistence/GradeMapper.java
+++ src/main/java/ch/hearc/ig/guideresto/persistence/GradeMapper.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class GradeMapper extends AbstractMapper<Grade> {
+    // on pourrait faire la meme avec les criteres d'évaluation mais c'est un peu inutile
     public Set<Grade> findByEvaluation(EntityManager em, CompleteEvaluation ce) {
         String jpqlQuery = "SELECT gra FROM Grade gra WHERE gra.evaluation = :evaluation";
         TypedQuery<Grade> query = em.createQuery(jpqlQuery, Grade.class);
