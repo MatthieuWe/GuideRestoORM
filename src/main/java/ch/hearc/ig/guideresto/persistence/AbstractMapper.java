9388f42a5a6b51c486699edb6747e3815880f177
Matthieu Wehrli <matthieu.wehrli@he-arc.ch>
2025-12-09 09:06:13 +0100

    added find methods for basic evaluations

--- src/main/java/ch/hearc/ig/guideresto/persistence/AbstractMapper.java
+++ src/main/java/ch/hearc/ig/guideresto/persistence/AbstractMapper.java
@@ -1,6 +1,7 @@
 package ch.hearc.ig.guideresto.persistence;
 
 import ch.hearc.ig.guideresto.business.*;
+import jakarta.persistence.EntityManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,45 +9,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 
 public abstract class AbstractMapper<T extends IBusinessObject> {
 
     protected static final Logger logger = LogManager.getLogger();
-    protected Map<Integer, IBusinessObject> cache = new HashMap<>();
-
-    public abstract T findById(int id);
-    public abstract Set<T> findAll();
-    public abstract T create(T object);
-    public abstract boolean update(T object);
+    public abstract Set<T> findAll(EntityManager em);
     public abstract boolean delete(T object);
-    public abstract boolean deleteById(int id);
 
-    protected abstract String getSequenceQuery();
-    protected abstract String getExistsQuery();
     protected abstract String getCountQuery();
-
-    /**
-     * Vérifie si un objet avec l'ID donné existe.
-     * @param id the ID to check
-     * @return true si l'objet existe, false sinon
-     */
-    public boolean exists(int id) {
-        Connection connection = ConnectionUtils.getConnection();
-
-        try (PreparedStatement stmt = connection.prepareStatement(getExistsQuery())) {
-            stmt.setInt(1, id);
-
-            try (ResultSet rs = stmt.executeQuery()) {
-                return rs.next();
-            }
-        } catch (SQLException ex) {
-            logger.error("SQLException: {}", ex.getMessage());
-        }
-        return false;
-    }
 
     /**
      * Compte le nombre d'objets en base de données.
@@ -67,96 +38,4 @@ public abstract class AbstractMapper<T extends IBusinessObject> {
             return 0;
         }
     }
-
-    /**
-     * Obtient la valeur de la séquence actuelle en base de données
-     * @return Le nombre de villes
-     * @En cas d'erreur SQL
-     */
-    protected Integer getSequenceValue() {
-        Connection connection = ConnectionUtils.getConnection();
-
-        try (PreparedStatement stmt = connection.prepareStatement(getSequenceQuery());
-             ResultSet rs = stmt.executeQuery()) {
-
-            if (rs.next()) {
-                return rs.getInt(1);
-            }
-            return 0;
-        } catch (SQLException ex) {
-            logger.error("SQLException: {}", ex.getMessage());
-            return 0;
-        }
-    }
-
-    /**
-     * Vérifie si le cache est actuellement vide
-     * @return true si le cache ne contient aucun objet, false sinon
-     */
-    protected boolean isCacheEmpty() {
-        return this.cache.isEmpty();
-    }
-
-    /**
-     * Vide le cache
-     */
-    protected void resetCache() {
-        this.cache.clear();
-    }
-
-    /**
-     * Ajoute un objet au cache
-     * @param objet l'objet à ajouter
-     */
-    protected void addToCache(T objet) {
-        int id = objet.getId();
-        this.cache.put(id, objet);
-        /* Selon le cours:
-        / Notez que la présence en cache est vérifiée 2 fois (findById et addToCache)
-        / – On s’assure ainsi de ne pas créer de doublon en mémoire; mieux vaut prévenir que guérir
-        / -> non je suis pas d'accord. Une map ne peut pas avoir de doublon de clé K, un put remplace la V existante
-         */
-    }
-
-    /**
-     * Retire un objet du cache
-     * @param id l'ID de l'objet à retirer du cache
-     */
-    protected void removeFromCache(int id) {
-        this.cache.remove(id);
-    }
-
-
-    // Méthodes pour charger un restaurant sont utilisées par plusieurs mappers: eager load depuis les évaluations
-    protected Restaurant loadRestaurant(ResultSet rs) throws SQLException {
-        City city = new City(rs.getInt("num_ville"),
-                rs.getString("code_postal"),
-                rs.getString("nom_ville"));
-        return this.loadRestaurant(rs, city);
-    }
-
-    protected Restaurant loadRestaurant(ResultSet rs, City city) throws SQLException {
-        RestaurantType type = new RestaurantType(rs.getInt("num_type"),
-                rs.getString("libelle"),
-                rs.getString("desc_type"));
-        return this.loadRestaurant(rs, city, type);
-    }
-    protected Restaurant loadRestaurant(ResultSet rs, RestaurantType type) throws SQLException {
-        City city = new City(rs.getInt("num_ville"),
-                rs.getString("code_postal"),
-                rs.getString("nom_ville"));
-        return this.loadRestaurant(rs, city, type);
-    }
-    protected Restaurant loadRestaurant(ResultSet rs, City city, RestaurantType type) throws SQLException {
-        Localisation address = new Localisation(rs.getString("adresse"), city);
-        Restaurant resto = new Restaurant(
-                rs.getInt("num_resto"),
-                rs.getString("nom"),
-                rs.getString("desc_resto"),
-                rs.getString("site_web"),
-                address,
-                type);
-        return resto;
-    }
-
 }
--- src/main/java/ch/hearc/ig/guideresto/persistence/BasicEvaluationMapper.java
+++ src/main/java/ch/hearc/ig/guideresto/persistence/BasicEvaluationMapper.java
@@ -1,235 +1,43 @@
 package ch.hearc.ig.guideresto.persistence;
 
 import ch.hearc.ig.guideresto.business.BasicEvaluation;
-import ch.hearc.ig.guideresto.business.City;
 import ch.hearc.ig.guideresto.business.Evaluation;
 import ch.hearc.ig.guideresto.business.Restaurant;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BasicEvaluationMapper extends AbstractMapper<BasicEvaluation> {
-    private final Connection connection ;
 
-    public BasicEvaluationMapper(Connection connection) {
-        this.connection = connection;
-    }
+    public Set<Evaluation> findByRestaurant(EntityManager em, Restaurant resto) {
+        String jpqlQuery = "SELECT be FROM BasicEvaluation be WHERE be.restaurant = :restaurant";
+        TypedQuery<BasicEvaluation> query = em.createQuery(jpqlQuery, BasicEvaluation.class);
+        query.setParameter("restaurant", resto);
+        Set<Evaluation> basicEvaluations = query.getResultStream()
+                .collect(Collectors.toSet());
 
-    @Override
-    public BasicEvaluation findById(int id) {
-        BasicEvaluation basicEvaluation = null;
-        if (super.cache.containsKey(id)) {
-            basicEvaluation = (BasicEvaluation) super.cache.get(id);
-        } else {
-            try {
-                PreparedStatement s = connection.prepareStatement(
-                    "SELECT l.numero num_like, l.appreciation, l.date_eval, l.adresse_ip" +
-                            " r.numero num_resto, r.nom, r.description desc_resto, r.site_web," +
-                            " r.adresse, v.numero num_ville, v.nom_ville, v.code_postal," +
-                            " t.numero num_type, t.libelle, t.description desc_type" +
-                            " FROM likes l" +
-                            " INNER JOIN restaurants r ON l.fk_rest = r.numero" +
-                            " INNER JOIN villes v ON r.fk_ville = v.numero" +
-                            " INNER JOIN types_gastronomiques t ON r.fk_type = t.numero" +
-                            " WHERE l.numero = ?");
-                s.setInt(1, id);
-                ResultSet rs = s.executeQuery();
-
-                if (rs.next()) {
-                    Boolean appreciation;
-                    if ("T" == rs.getString("appreciation")) {
-                        appreciation=true;
-                    } else {
-                        appreciation=false;
-                    }
-                    Restaurant restaurant = super.loadRestaurant(rs);
-                    basicEvaluation = new BasicEvaluation(
-                        rs.getInt("numero"),
-                        rs.getDate("date_eval"),
-                        restaurant,
-                        appreciation,
-                        rs.getString("adresse_ip")
-                    );
-                    super.addToCache(basicEvaluation);
-                } else {
-                    logger.error("No basic evaluation found");
-                }
-                rs.close();
-            } catch (SQLException e) {
-                logger.error("SQLException:{}", e.getMessage());
-            }
-        }
-        return basicEvaluation;
-    }
-    public Set<Evaluation> findForRestaurant(Restaurant resto) {
-        Set<Evaluation> basicEvaluations = new HashSet<>();
-        try {
-            PreparedStatement s = connection.prepareStatement("SELECT * FROM likes WHERE fk_rest = ?");
-            s.setInt(1, resto.getId());
-            ResultSet rs = s.executeQuery();
-
-            while (rs.next()) {
-                int id = rs.getInt("numero");
-                if (super.cache.containsKey(id)) {
-                    basicEvaluations.add((BasicEvaluation) super.cache.get(id));
-                } else {
-                    Boolean appreciation;
-                    if ("T" == rs.getString("appreciation")) {
-                        appreciation=true;
-                    } else {
-                        appreciation=false;
-                    }
-                    BasicEvaluation eval = new BasicEvaluation(
-                            rs.getInt("numero"),
-                            rs.getDate("date_eval"),
-                            resto,
-                            appreciation,
-                            rs.getString("adresse_ip")
-                    );
-                    basicEvaluations.add(eval);
-                    super.addToCache(eval);
-                }
-            }
-            rs.close();
-        } catch (SQLException e) {
-            logger.error("SQLException:{}", e.getMessage());
-        }
         return basicEvaluations;
     }
 
     @Override
-    public Set<BasicEvaluation> findAll() {
-        Set<BasicEvaluation> basicEvaluations = new HashSet<>();
-        super.resetCache();
-        try {
-            PreparedStatement s = connection.prepareStatement(
-                    "SELECT l.numero num_like, l.appreciation, l.date_eval, l.adresse_ip" +
-                    " r.numero num_resto, r.nom, r.description desc_resto, r.site_web," +
-                    " r.adresse, v.numero num_ville, v.nom_ville, v.code_postal," +
-                    " t.numero num_type, t.libelle, t.description desc_type" +
-                    " FROM likes l" +
-                    " INNER JOIN restaurants r ON l.fk_rest = r.numero" +
-                    " INNER JOIN villes v ON r.fk_ville = v.numero" +
-                    " INNER JOIN types_gastronomiques t ON r.fk_type = t.numero"
-            );
-            ResultSet rs = s.executeQuery();
-            while (rs.next()) {
-                Boolean appreciation;
-                if ("T" == rs.getString("appreciation")) {
-                	appreciation=true;
-                } else {
-                	appreciation=false;
-                }
-                Restaurant restaurant = super.loadRestaurant(rs);
-                BasicEvaluation basicEvaluation = new BasicEvaluation(
-                        rs.getInt("num_like"),
-                        rs.getDate("date_eval"),
-                        restaurant,
-                        appreciation, //🥰🥰🥰🥰🥰🥰 ça marchera !
-                        rs.getString("adresse_ip")
-                );
-                basicEvaluations.add(basicEvaluation);
-                super.addToCache(basicEvaluation);
-            }
-            rs.close();
-        } catch (SQLException e) {
-            logger.error("SQLException: {}", e.getMessage());
-        }
+    public Set<BasicEvaluation> findAll(EntityManager em) {
+        String jpqlQuery = "SELECT be FROM BasicEvaluation be";
+        Set<BasicEvaluation> basicEvaluations = em.createQuery(jpqlQuery, BasicEvaluation.class)
+                .getResultStream()
+                .collect(Collectors.toSet());
+
         return basicEvaluations;
-    }
-
-    @Override
-    public BasicEvaluation create(BasicEvaluation basicEvaluation) {
-        try {
-            String generatedColumns[] = {"numero"};
-            PreparedStatement s = connection.prepareStatement(
-                    "INSERT INTO likes (date_eval, fk_rest, appreciation, adresse_ip)" +
-                            "VALUES (?, ?, ?, ?)",
-                    generatedColumns);
-            s.setDate(1, new java.sql.Date(basicEvaluation.getVisitDate().getTime()));
-            s.setInt(2, basicEvaluation.getRestaurant().getId());
-            s.setString(3, basicEvaluation.getLikeRestaurant() ? "T" : "F"); //ewewewewe je veux pas
-            s.setString(4, basicEvaluation.getIpAddress());
-            s.executeUpdate();
-            ResultSet rs = s.getGeneratedKeys();
-            if (rs.next()) {
-                basicEvaluation.setId(rs.getInt(1));
-                super.addToCache(basicEvaluation);
-            } else {
-                logger.warn("Failed to insert basic evaluation into the table : ");
-            }
-            rs.close();
-            connection.commit(); //ne pas oublier les commits sinon... ça s'efface!!
-        } catch (SQLException e) {
-            logger.error("SQLException: {}", e.getMessage());
-        }
-        return basicEvaluation;
-    }
-
-    @Override
-    public boolean update(BasicEvaluation basicEvaluation) {
-        int affectedRows = 0;
-        try {
-            PreparedStatement s = connection.prepareStatement(
-                    "UPDATE likes" +
-                            " SET date_eval = ?, fk_rest = ?, appreciation = ?, adresse_ip = ?" +
-                            " WHERE numero = ?");
-            s.setDate(1, new java.sql.Date(basicEvaluation.getVisitDate().getTime()));
-            s.setInt(2, basicEvaluation.getRestaurant().getId());
-
-            s.setString(3, basicEvaluation.getLikeRestaurant() ? "T" : "F"); //ew ew ew ew j'aime pas quand connection'est écrit comme ça 🤮🤮🤮🤮
-            s.setString(4, basicEvaluation.getIpAddress());
-            s.setInt(5, basicEvaluation.getId());
-            affectedRows = s.executeUpdate();
-            connection.commit();
-            super.addToCache(basicEvaluation);
-        } catch (SQLException e) {
-            logger.error("SQLException: {}", e.getMessage());
-        }
-        return affectedRows > 0;
     }
 
     @Override
     public boolean delete(BasicEvaluation basicEvaluation) {
-        return this.deleteById(basicEvaluation.getId());
-    }
-
-    @Override
-    public boolean deleteById(int id) {
-        int affectedRows = 0;
-        try {
-            PreparedStatement s = connection.prepareStatement(
-                    "DELETE FROM likes WHERE numero = ?");
-            s.setInt(1, id);
-            affectedRows = s.executeUpdate();
-            connection.commit();
-        } catch (SQLException e) {
-            logger.error("SQLException: {}", e.getMessage());
-        }
-        if(affectedRows > 0) {
-            super.removeFromCache(id);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    protected String getSequenceQuery() {
-        return "SELECT seq_likes.NextVal FROM dual";
-    }
-
-    @Override
-    protected String getExistsQuery() {
-        return "SELECT numero FROM likes WHERE numero = ?";
+        return true;
     }
 
     @Override
     protected String getCountQuery() {
-        return "SELECT Count(*) FROM likes";
+        return "SELECT Count(be) FROM BasicEvaluation be";
     }
 }
