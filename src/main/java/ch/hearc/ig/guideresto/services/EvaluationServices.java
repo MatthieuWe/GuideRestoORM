98942e8104df427ace0689731b77a5ffc117e4f6
Matthieu Wehrli <matthieu.wehrli@he-arc.ch>
2026-01-06 09:01:36 +0100

    fixed add complete evaluation transaction

--- src/main/java/ch/hearc/ig/guideresto/services/EvaluationServices.java
+++ src/main/java/ch/hearc/ig/guideresto/services/EvaluationServices.java
@@ -82,25 +82,21 @@ public class EvaluationServices {
         tx.commit();
     }
 
-    /*
-    * TODO utiliser la transaction plus élégante vue en cours -> partout
-     */
     public Boolean addCompleteEvaluation(Restaurant restaurant, String comment, String username, Map<EvaluationCriteria, Integer> grades) {
-
         try {
-            EntityTransaction tx = em.getTransaction();
+            JpaUtils.inTransaction(em-> {
+                CompleteEvaluation eval = new CompleteEvaluation(new Date(), restaurant, comment, username);
+                em.persist(eval);
+                eval.setGrades(new HashSet<>());
 
-            tx.begin();
-            CompleteEvaluation eval = new CompleteEvaluation(new Date(), restaurant, comment, username);
-            em.persist(eval);
-
-            // TODO est-ce qu'on a tous les critères existants en DB ni plus ni moins? - question de concurrence a voir plus tard ici
-            for (EvaluationCriteria ec : grades.keySet()) {
-                Integer note = grades.get(ec);
-                Grade grade = new Grade(note, eval, ec);
-                em.persist(grade);
-            }
-            tx.commit();
+                for (EvaluationCriteria ec : grades.keySet()) {
+                    Integer note = grades.get(ec);
+                    Grade grade = new Grade(note, eval, ec);
+                    em.persist(grade);
+                    eval.getGrades().add(grade);
+                }
+                restaurant.getEvaluations().add(eval);
+            });
         } catch (Exception e) {
             logger.error("Error while adding complete evaluation: " + e.getMessage());
             return false;
